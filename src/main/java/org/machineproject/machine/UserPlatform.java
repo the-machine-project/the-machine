5a9d10934c2f991c08e97dc22fbc21eb9e505122
Xnart <bl4de44@gmail.com>
2016-05-31 23:28:08 +0300

    detecting user's operating system

---
+++ src/main/java/org/machineproject/machine/UserPlatform.java
@@ -0,0 +1,12 @@
+package org.machineproject.machine;
+
+/**
+ * Created by xnart on 31.05.2016.
+ */
+
+public final class UserPlatform {
+
+    public static final String OS_NAME = System.getProperty("os.name");
+    public static final String OS_ARCH = System.getProperty("os.arch");
+
+}
